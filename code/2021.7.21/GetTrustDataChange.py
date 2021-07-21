b4e8193758014f389e89db8b3a21dce8af3d7a8e
KwonMoonjeong <87647372+KwonMoonjeong@users.noreply.github.com>
2021-07-21 15:50:08 +0900

    Update GetTrustDataChange.py

--- code/2021.7.21/GetTrustDataChange.py
+++ code/2021.7.21/GetTrustDataChange.py
@@ -1,5 +1,5 @@
 # 공공 데이터 - 위탁의료기관 데이터 정제
-# 권문정, 황성호 / 2021.07.21
+# 1-A조(권문정, 황성호) 2021.07.21
 
 def GetTrustDataChange(resultJsonData, changeDataName):
     for iCount in range(0, resultJsonData['currentCount']):
