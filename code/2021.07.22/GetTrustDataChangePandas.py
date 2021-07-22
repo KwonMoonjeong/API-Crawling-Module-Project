0d13cc13e5efc9fe329c45fdab45d4711cddcfea
KwonMoonjeong <87647372+KwonMoonjeong@users.noreply.github.com>
2021-07-22 16:21:48 +0900

    Update GetTrustDataChangePandas.py

--- code/2021.07.22/GetTrustDataChangePandas.py
+++ code/2021.07.22/GetTrustDataChangePandas.py
@@ -6,7 +6,7 @@ from pandas import json_normalize
 import numpy as np
 
 
-def GetTrustDataChange(resultJsonData, changeDataName):
+def GetTrustDataChangePandas(resultJsonData, changeDataName):
 
     dataOfResult = json_normalize(resultJsonData['data']) # "data"의 값들을 Pandas 데이터프레임으로 추출
     dataOfResult = dataOfResult[['orgnm', 'orgZipaddr', 'orgTlno']]    # 기관명, 주소, 전화번호 컬럼 추출
