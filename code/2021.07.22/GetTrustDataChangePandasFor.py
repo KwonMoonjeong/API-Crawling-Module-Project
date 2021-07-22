6adefd09cc932d87d057c72fbea4125cff27240b
KwonMoonjeong <87647372+KwonMoonjeong@users.noreply.github.com>
2021-07-22 14:10:27 +0900

    Create GetTrustDataChangePandasFor.py
    Pandas 프레임워크와 for문을 활용하여 위탁의료기관 데이터를 정제하는 코드이다

---
+++ code/2021.07.22/GetTrustDataChangePandasFor.py
@@ -0,0 +1,16 @@
+def GetTrustDataChange(resultJsonData, changeDataName):
+
+    dataOfResult = json_normalize(resultJsonData['data']) # "data"의 값들을 Pandas 데이터프레임으로 추출
+    dataOfResult = dataOfResult[['orgnm', 'orgZipaddr', 'orgTlno']]    # 기관명, 주소, 전화번호 컬럼 추출
+    dataOfResult = dataOfResult.rename(columns={'orgnm' : 'hName',      # 컬럼명 변경
+                                                'orgZipaddr' : 'hAddress',
+                                                 'orgTlno' : 'hNum'}, inplace=False)
+
+    dataOfResult['hLon'] = np      # 경도와 위도를 넣어줄 빈 컬럼 2개 생성
+    dataOfResult['hLat'] = np
+
+    for iCount in range(0, resultJsonData['currentCount']):     # 좌표 가져오기
+        hLon, hLat = GetGeoLocationData(resultJsonData['data'][iCount]['orgZipaddr'])
+
+        dataOfResult.loc[iCount,'hLon'] = hLon      # 경도와 위도 컬럼에 좌표 채우기
+        dataOfResult.loc[iCount,'hLat'] = hLat
